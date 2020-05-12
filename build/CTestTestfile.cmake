7beabed3b389326211534198fb91804750b59d89
Praneeth Eddu <praneetheddu@gmail.com>
2020-05-12 18:45:29 -0400

    catkin_ws setup

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pele/Documents/repos/Sky_Blast/src
+# Build directory: /home/pele/Documents/repos/Sky_Blast/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
